6b2d0057fc533422679121d26565947415d90eb7
Adarsh <adarsh@haikujam.com>
2021-07-07 01:52:07 +0530

    added valiadtion to req data(for registering new user) using express-validator/check

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check')
 
-// @route    GET api/users
-// @desc     Test route
+// @route    POST api/users
+// @desc     Register User
 // @access   Public
-router.get('/', (req,res) => {
-    res.send('User Route')
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more charaters').isLength({ min: 6 })
+], (req,res) => {
+    const errors = validationResult(req);
+    console.log(typeof(errors));
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    res.send("User Router");
 });
 
 
