dc37b7f394aeab85abdebd3a9f1fab2c2f0d76f5
Adarsh <adarsh@haikujam.com>
2021-07-15 03:02:59 +0530

    code templating config files

--- config/db.js
+++ config/db.js
@@ -3,23 +3,20 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.log(err.message);
 
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB Connected');
-
-    } catch(err) {
-
-        console.log(err.message);
-
-        // Code to exit process with failure
-        process.exit(1);
-    }
-}
+    // Code to exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
