e50922f86e277fb8ddbd45a5a2dd0a6f80fd0397
Adarsh <adarsh@haikujam.com>
2021-07-15 03:00:19 +0530

    templating auth.js router-apis file

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,68 +11,72 @@ const User = require('../../models/User');
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
-router.get('/', auth, async (req,res) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-    } catch(err) {
-        console.log(err.message);
-        res.status(500).send({
-            msg: 'Server Error'
-        })
-    }
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({
+      msg: 'Server Error',
+    });
+  }
 });
 
 // @route    POST api/auth
 // @desc     Authenticate User & Get Token
 // @access   Public
-router.post('/', [
+router.post(
+  '/',
+  [
     check('email', 'Please enter a valid email').isEmail(),
-    check('password', 'Password is required').exists()
-], async (req,res) => {
-
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
 
     try {
+      let user = await User.findOne({ email });
 
-        let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentails' }] });
+      }
 
-        if(!user) {
-            return res.status(400).json({ errors: [ { msg: "Invalid Credentails" } ] })
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentails' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
         }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if(!isMatch) {
-            return res.status(400).json({ errors: [ { msg: "Invalid Credentails" } ] })
-        }
-
-        const payload = {
-            user: {
-                id: user.id
-            }
-        }
-
-        jwt.sign(
-            payload,
-            config.get('jwtSecret'),
-            { expiresIn: 360000 },
-            (err, token) => {
-                if(err) throw err;
-                res.json({ token });
-            }
-        )
-
-    }  catch(err) {
-        console.log(err.message);
-        res.status(500).send('Server Error');
-    }    
-});
-
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
